826bfc47509aed55f88a246bb4d29836dd1406a3
ClayF143 <48032691+ClayF143@users.noreply.github.com>
2021-02-22 20:14:39 -0500

    should be finished

--- src/Solver_2x2x2/Cube_2x2x2.java
+++ src/Solver_2x2x2/Cube_2x2x2.java
@@ -21,6 +21,8 @@ public class Cube_2x2x2
 		   4 numbered colors represent the order of top left, top right,
 		   bottom left, bottom right */
 		/* For example: all the 1s indicate red, the 2s indicate white, etc.*/
+		
+		// white 1, red 2, green 3, yellow 4, blue 5, orange 6
 		cube = new int [] []   {{1, 1, 1, 1}, 
 								{2, 2, 2, 2},
 								{3, 3, 3, 3},
@@ -28,10 +30,13 @@ public class Cube_2x2x2
 								{5, 5, 5, 5},
 								{6, 6, 6, 6}};
 								
-		/* testing stuff
-		cube[0][1] = 2;
-		cube[0][2] = 3;
-		cube[0][3] = 4; */
+		// testing stuff
+		cube = new int [] []   {{1,3,1,3}, 
+								{3,4,6,6},
+								{2,2,3,4},
+								{1,5,2,2},
+								{6,6,1,5},
+								{4,4,5,5}};
 	}
 	
 	public void randomize(int k)
@@ -41,23 +46,29 @@ public class Cube_2x2x2
 		
 		for(int i = 0; i < k; i++)
 		{
-			// generate a random number between 0 and 6
+			//System.out.println("Randomizing ");
+			//System.out.println(i);
+			// generate a random number between 0 and 6, and one for how many turns to make
 			int randomInt = ThreadLocalRandom.current().nextInt(1,7);
+			int turns = ThreadLocalRandom.current().nextInt(1,4);
+			
+			//System.out.println("randomint - " + String.valueOf(randomInt));
+			//System.out.println("turns - " + String.valueOf(turns));
 			// testing
 			// System.out.println(randomInt);
 			switch(randomInt)
 			{
-				case 1: this.top(1);
+				case 1: this.top(turns);
 					break;
-				case 2: this.front(1);
+				case 2: this.front(turns);
 					break;
-				case 3: this.right(1);
+				case 3: this.right(turns);
 					break;
-				case 4: this.back(1);
+				case 4: this.back(turns);
 					break;
-				case 5: this.left(1);
+				case 5: this.left(turns);
 					break;
-				case 6: this.down(1);
+				case 6: this.down(turns);
 					break;
 				default: System.out.println("Error - randomize method");
 			}
@@ -74,17 +85,40 @@ public class Cube_2x2x2
 		return correct when all faces have the same color so [1,1,1,1],
 		[3,3,3,3], [2,2,2,2], etc
 		*/
-		return false;
+		for(int [] face : this.cube)
+		{
+			int color = face[0];
+			for(int i = 1; i<face.length; i++)
+			{
+				if(face[i] != color)
+					return false;
+			}
+		}
+		return true;
 	}
 	
 	public Cube_2x2x2 clone()
 	{
 		// wip
 		// creates a clone of this object and output it
-		return null;
+		
+		Cube_2x2x2 clone = new Cube_2x2x2();
+		for(int i = 0; i < this.cube.length; i++)
+		{
+			clone.cube[i] = this.cube[i].clone();
+		}
+		return clone;
 	}
 	
-	public void top(int k)
+	/* NOTE:
+	 * If you want to look at any face, it's best to look at it as a 2-d square, top down.
+	 * Starting from the top face, if you want to look at the top, shift the front to be where the bottom face is
+	 * If you want to look at the bottom, shift the front to where the top face is.
+	 * If you want to look at the right, back, or left faces, just rotate horizontally.
+	 * This is important, especially with the bottom and back faces.
+	 */
+	
+	public void top(int k)	
 	{
 		// This function rotates the top face clockwise k times
 		
@@ -131,6 +165,34 @@ public class Cube_2x2x2
 		
 		for(int i = 0; i < k; i++)
 		{
+			// first rotate the actual face
+			int frontTopLeft = cube[1][0];
+			int frontTopRight = cube[1][1];
+			int frontBotLeft = cube[1][2];
+			int frontBotRight = cube[1][3];
+			cube[1][0] = frontBotLeft;
+			cube[1][1] = frontTopLeft;
+			cube[1][2] = frontBotRight;
+			cube[1][3] = frontTopRight;
+			
+			// then rotate the squares sharing an edge with the face
+			int topBotLeft = cube[0][2];
+			int topBotRight = cube[0][3];
+			int rightTopLeft = cube[2][0];
+			int rightBotLeft = cube[2][2];
+			int botTopLeft = cube[5][0];
+			int botTopRight = cube[5][1];
+			int leftTopRight = cube[4][1];
+			int leftBotRight = cube[4][3];
+			cube[0][2] = leftBotRight;
+			cube[0][3] = leftTopRight;
+			cube[2][0] = topBotLeft;
+			cube[2][2] = topBotRight;
+			cube[5][0] = rightBotLeft;
+			cube[5][1] = rightTopLeft;
+			cube[4][1] = botTopLeft;
+			cube[4][3] = botTopRight;
+			
 			
 		}
 	}
@@ -141,7 +203,31 @@ public class Cube_2x2x2
 		
 		for(int i = 0; i < k; i++)
 		{
+			int rightTopLeft = cube[2][0];
+			int rightTopRight = cube[2][1];
+			int rightBotLeft = cube[2][2];
+			int rightBotRight = cube[2][3];
+			cube[2][0] = rightBotLeft;
+			cube[2][1] = rightTopLeft;
+			cube[2][2] = rightBotRight;
+			cube[2][3] = rightTopRight;
 			
+			int topTopRight = cube[0][1];
+			int topBotRight = cube[0][3];
+			int frontTopRight = cube[1][1];
+			int frontBotRight = cube[1][3];
+			int botTopRight = cube[5][1];
+			int botBotRight = cube[5][3];
+			int backTopLeft = cube[3][0];
+			int backBotLeft = cube[3][2];
+			cube[0][1] = frontTopRight;
+			cube[0][3] = frontBotRight;
+			cube[1][1] = botTopRight;
+			cube[1][3] = botBotRight;
+			cube[5][1] = backBotLeft;
+			cube[5][3] = backTopLeft;
+			cube[3][0] = topBotRight;
+			cube[3][2] = topTopRight;
 		}
 	}
 	
@@ -151,7 +237,31 @@ public class Cube_2x2x2
 		
 		for(int i = 0; i < k; i++)
 		{
+			int backTopLeft = cube[3][0];
+			int backTopRight = cube[3][1];
+			int backBotLeft = cube[3][2];
+			int backBotRight = cube[3][3];
+			cube[3][0] = backBotLeft;
+			cube[3][1] = backTopLeft;
+			cube[3][2] = backBotRight;
+			cube[3][3] = backTopRight;
 			
+			int topTopLeft = cube[0][0];
+			int topTopRight = cube[0][1];
+			int rightTopRight = cube[2][1];
+			int rightBotRight = cube[2][3];
+			int botBotLeft = cube[5][2];
+			int botBotRight = cube[5][3];
+			int leftTopLeft = cube[4][0];
+			int leftBotLeft = cube[4][2];
+			cube[0][0] = rightTopRight;
+			cube[0][1] = rightBotRight;
+			cube[2][1] = botBotRight;
+			cube[2][3] = botBotLeft;
+			cube[5][2] = leftTopLeft;
+			cube[5][3] = leftBotLeft;
+			cube[4][0] = topTopRight;
+			cube[4][2] = topTopLeft;
 		}
 	}
 	
@@ -161,6 +271,32 @@ public class Cube_2x2x2
 		
 		for(int i = 0; i < k; i++)
 		{
+			int leftTopLeft = cube[4][0];
+			int leftTopRight = cube[4][1];
+			int leftBotLeft = cube[4][2];
+			int leftBotRight = cube[4][3];
+			cube[4][0] = leftBotLeft;
+			cube[4][1] = leftTopLeft;
+			cube[4][2] = leftBotRight;
+			cube[4][3] = leftTopRight;
+			
+			int topTopLeft = cube[0][0];
+			int topBotLeft = cube[0][2];
+			int frontTopLeft = cube[1][0];
+			int frontBotLeft = cube[1][2];
+			int botTopLeft = cube[5][0];
+			int botBotLeft = cube[5][2];
+			int backTopRight = cube[3][1];
+			int backBotRight = cube[3][3];
+			cube[0][0] = backBotRight;
+			cube[0][2] = backTopRight;
+			cube[1][0] = topTopLeft;
+			cube[1][2] = topBotLeft;
+			cube[5][0] = frontTopLeft;
+			cube[5][2] = frontBotLeft;
+			cube[3][1] = botBotLeft;
+			cube[3][3] = botTopLeft;
+			
 			
 		}
 	}
@@ -171,7 +307,31 @@ public class Cube_2x2x2
 		
 		for(int i = 0; i < k; i++)
 		{
+			int botTopLeft = cube[5][0];
+			int botTopRight = cube[5][1];
+			int botBotLeft = cube[5][2];
+			int botBotRight = cube[5][3];
+			cube[5][0] = botBotLeft;
+			cube[5][1] = botTopLeft;
+			cube[5][2] = botBotRight;
+			cube[5][3] = botTopRight;
 			
+			int frontBotLeft = cube[1][2];
+			int frontBotRight = cube[1][3];
+			int rightBotLeft = cube[2][2];
+			int rightBotRight = cube[2][3];
+			int backBotLeft = cube[3][2];
+			int backBotRight = cube[3][3];
+			int leftBotLeft = cube[4][2];
+			int leftBotRight = cube[4][3];
+			cube[1][2] = leftBotLeft;
+			cube[1][3] = leftBotRight;
+			cube[2][2] = frontBotLeft;
+			cube[2][3] = frontBotRight;
+			cube[3][2] = rightBotLeft;
+			cube[3][3] = rightBotRight;
+			cube[4][2] = backBotLeft;
+			cube[4][3] = backBotRight;
 		}
 	}
 	
@@ -183,7 +343,41 @@ public class Cube_2x2x2
 		{
 			for(int color : face)
 			{
-				System.out.print(color);
+				System.out.print(String.valueOf(color) +" ");
+			}
+			System.out.print("\n");
+		}
+	}
+	
+	public void printColor()
+	{
+		// This function prints every color in the cube
+		System.out.println("Printing Cube:\n");
+		for(int [] face: cube)
+		{
+			for(int color: face)
+			{
+				switch(color)
+				{
+				case 1:
+					System.out.print("White  ");
+					break;
+				case 2:
+					System.out.print("Red    ");
+					break;
+				case 3:
+					System.out.print("Green  ");
+					break;
+				case 4:
+					System.out.print("Yellow ");
+					break;
+				case 5:
+					System.out.print("Blue   ");
+					break;
+				case 6:
+					System.out.print("Orange ");
+					break;
+				}
 			}
 			System.out.print("\n");
 		}
@@ -191,9 +385,13 @@ public class Cube_2x2x2
 	
 	public static void main(String[] args)
 	{
+		System.out.println("white 1, red 2, green 3, yellow 4, blue 5, orange 6");
 		// Stuff for testing
 		Cube_2x2x2 cube = new Cube_2x2x2();
-		cube.top(1);
+		Cube_2x2x2 clone = cube.clone();
+		cube.down(1);
+		//cube.printColor();
 		cube.print();
+		cube.printColor();
 	}
 }
