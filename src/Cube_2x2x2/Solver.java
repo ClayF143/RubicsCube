e1e978464e2f60e5b1ff632816cbc12fd3568954
ClayF143 <48032691+ClayF143@users.noreply.github.com>
2021-03-03 01:01:21 -0500

    cleaning up and filling out, still not done

--- src/Cube_2x2x2/Solver.java
+++ src/Cube_2x2x2/Solver.java
@@ -4,62 +4,62 @@ import java.util.PriorityQueue;
 
 public class Solver
 {
-	private PriorityQueue<Node> frontier; // not an array, I haven't looked up syntax for prio queues yet
+	private PriorityQueue<Node> frontier;
 	private Node root;
 	
-	public boolean solve()
+	public void IDAStar()
 	{
 		System.out.println("Solving Cube, please standby.");
+		// this is talking about the threshold that's printed at the bottom of the loop
 		System.out.println("The last of these numbers is proportional to how easy the solution is.");
 		System.out.println("Generally, the harder the solution, the longer the computation time.");
 		
-		int threshold = root.h;
-		boolean unsolved = true;
-		
-		while(unsolved)
+		int threshold = root.h; // this equals root.f, fyi
+		// this outer loop is present so that the threshold can be increased over as many iterations as required
+		while(true)
 		{
+			// search for a solution with f < threshold
+			frontier.add(root);
 			while(!frontier.isEmpty())
 			{
 				Node n = frontier.poll();
 				
 				if(n.isSolved())
 				{
-					// print path
-					
-					return true;
+					// Once a solution is found, print out that solution and end the search
+					System.out.println("Solution Path:");
+					n.printPath();
+					return;
 				}
 				
-				Node [] children = n.generateChildren();
+				Node [] children = n.generateChildren(); // might need to turn this into a list
 				for(Node child: children)
 				{
 					if(child.f < threshold)
 					{
-						// add to priority queue
+						frontier.add(child);
 					}
 				}
 			}
 			System.out.println(threshold);
 			threshold++;
-			
 		}
-		
-		return false;
-		
-		
 	}
 	
 	private class Node implements Comparable<Node>
 	{
 		public Node parent;
-		public Cube_2x2x2 cube;
+		public String turn;
+		public Cube_2x2x2 state;
 		public int h, g, f;
 		
-		public Node(Cube_2x2x2 cube, Node parent)
+		public Node(Cube_2x2x2 state, Node parent, String turn)
 		{
-			this.cube = cube;
+			this.state = state;
 			this.parent = parent;
-			// set h, g, and f
+			this.turn = turn;
 			
+			// set h, g, and f
 			setH();
 			
 			if(parent == null)
@@ -77,20 +77,32 @@ public class Solver
 		}
 		
 		// heuristic function
+		// hamming distance
 		public void setH()
 		{
+			// wip
+			// pick a corner to set
+			// count out of place squares
+			// sum and divide by 8
 			h = 0;
 		}
 		
 		public boolean isSolved()
 		{
-			return cube.isSolved();
+			return state.isSolved();
 		}
 
 		@Override
 		public int compareTo(Node n)
 		{
 			return this.f - n.f;
+		}
+		
+		public void printPath()
+		{
+			if(parent != null)
+				parent.printPath();
+				System.out.println(turn);
 		}
 	}
 }
